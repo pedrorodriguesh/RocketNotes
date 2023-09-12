2f2a224a7946820a474ad040061ce27cd753cf83
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-12 14:17:14 -0300

    comments

--- RocketNotes/src/main.jsx
+++ RocketNotes/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details/Details'
-import { ThemeProvider } from 'styled-components'
+
+import { Details } from './pages/Details/Details' // componente importado
+
+import { ThemeProvider } from 'styled-components' // tema global da página
 import theme from "./styles/theme"
 import GlobalStyles from './styles/global'
 
