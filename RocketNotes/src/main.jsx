87d91b0ad31d5bf3f3f677655949bda97cce1bbc
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-12 16:50:23 -0300

    testing keyboard

--- RocketNotes/src/main.jsx
+++ RocketNotes/src/main.jsx
@@ -7,12 +7,14 @@ import { ThemeProvider } from 'styled-components' // tema global da página
 import theme from "./styles/theme"
 import GlobalStyles from './styles/global'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}> 
       <GlobalStyles />
         <Details />
     </ThemeProvider>
   </React.StrictMode>,
 )
+
+
+
