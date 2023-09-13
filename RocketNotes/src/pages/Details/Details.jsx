2a564303706343a1600e01938c970bd53145be89
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-13 17:08:14 -0300

    section component

--- RocketNotes/src/pages/Details/Details.jsx
+++ RocketNotes/src/pages/Details/Details.jsx
@@ -1,16 +1,25 @@
-import { Container } from "./styles"
-import { Button } from "../../components/Button"
-import { Header } from "../../components/Header"
+import { Container, Links } from './styles';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
 
-export function Details(){
-  
-  return(
-    <Container>
-      <Header>
-        
-      </Header>
-      <Button title="Voltar" />
-    
-    </Container>
-  )
-} 
+export function Details() {
+    return (
+        <Container>
+            <Header></Header>
+
+            <Section title="Links úteis">
+                <Links>
+                    <li>
+                        <a href="#">https://www.github.com/pedrorodriguesh</a>
+                    </li>
+                    <li>
+                        <a href="#">https://rocketseat.com.br/</a>
+                    </li>
+                </Links>
+            </Section>
+
+            <Button title="Voltar" />
+        </Container>
+    );
+}
