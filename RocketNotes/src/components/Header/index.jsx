5fa89cb5455b0bddfa94aa5c02da806f6ffc1d53
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-13 16:08:32 -0300

    component header

--- RocketNotes/src/components/Header/index.jsx
+++ RocketNotes/src/components/Header/index.jsx
@@ -1,12 +1,22 @@
-import { Container } from './styles.js'
+import { Container, Profile, Logout } from './styles.js'
+import { RiShutDownLine } from 'react-icons/ri'
 
 
 export function Header() {
-  return (
+  return(
     <Container>
-      <img src="" alt="" />
+      <Profile>
+        <img src="https://github.com/pedrorodriguesh.png" alt="Profile photo" />
 
+        <div>
+          <span>Bem-Vindo,</span>
+          <strong>Pedro Henrique Rodrigues</strong>
+        </div>
+      </Profile>
 
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
     </Container>
   )
 }
--- RocketNotes/src/components/Header/styles.js
+++ RocketNotes/src/components/Header/styles.js
@@ -1,7 +1,55 @@
 import styled from 'styled-components'
 
 export const Container = styled.header`
-  padding: 20px 45px;
-  border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700}
+  grid-area: header;
+
+  height: 105px;
+  width: 100%;
+  border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+  display: flex;
+  justify-content: space-between;
+
+  padding: 0 80px;
+`
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+
+  > img {
+    width: 56px;
+    height: 56px;
+    border-radius: 99999px;
+  }
+
+  > div{
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 24px;
+
+    span{
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+
+    strong{
+      font-size: 18px;
+      color: white;
+    }
+  }
+
   
+
+`
+
+
+export const Logout = styled.button`
+  border: none;
+  background: none;
+
+  > svg {
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+    font-size: 36px;
+  }
 `
