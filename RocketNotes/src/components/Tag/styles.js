7070c9efb6bf574de244183f499efdefd003a6d1
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-13 17:46:52 -0300

    tag font weigth change

--- RocketNotes/src/components/Tag/styles.js
+++ RocketNotes/src/components/Tag/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.span`
     font-size: 12px;
+    font-weight: 500;
     padding: 5px 14px;
     border-radius: 5px;
     margin-right: 6px;
