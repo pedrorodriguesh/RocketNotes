554b6eec9bd04968b4fd5b22628bfd53deda97f0
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-13 16:27:12 -0300

    prettier settings

--- RocketNotes/src/components/Header/styles.js
+++ RocketNotes/src/components/Header/styles.js
@@ -1,56 +1,52 @@
-import styled from 'styled-components'
-const name = "my name"
+import styled from 'styled-components';
+
 
 export const Container = styled.header`
-  grid-area: header;
+    grid-area: header;
 
-  height: 105px;
-  width: 100%;
-  border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
+    height: 105px;
+    width: 100%;
+    border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
 
-  display: flex;
-  justify-content: space-between;
+    display: flex;
+    justify-content: space-between;
 
-  padding: 0 80px;
-`
+    padding: 0 80px;
+`;
 
 export const Profile = styled.div`
-  display: flex;
-  align-items: center;
-
-  > img {
-    width: 56px;
-    height: 56px;
-    border-radius: 99999px;
-  }
-
-  > div{
     display: flex;
-    flex-direction: column;
-    margin-left: 16px;
-    line-height: 24px;
+    align-items: center;
 
-    span{
-      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    > img {
+        width: 56px;
+        height: 56px;
+        border-radius: 99999px;
     }
 
-    strong{
-      font-size: 18px;
-      color: white;
+    > div {
+        display: flex;
+        flex-direction: column;
+        margin-left: 16px;
+        line-height: 24px;
+
+        span {
+            color: ${({ theme }) => theme.COLORS.GRAY_100};
+        }
+
+        strong {
+            font-size: 18px;
+            color: white;
+        }
     }
-  }
-
-  
-
-`
-
+`;
 
 export const Logout = styled.button`
-  border: none;
-  background: none;
+    border: none;
+    background: none;
 
-  > svg {
-    color: ${({ theme }) => theme.COLORS.GRAY_100};
-    font-size: 36px;
-  }
-`
+    > svg {
+        color: ${({ theme }) => theme.COLORS.GRAY_100};
+        font-size: 36px;
+    }
+`;
