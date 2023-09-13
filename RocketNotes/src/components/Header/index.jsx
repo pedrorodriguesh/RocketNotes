9716475e97582c487116964ceaadd310df62a4b4
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-13 15:37:08 -0300

    header component

---
+++ RocketNotes/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from './styles.js'
+
+
+export function Header() {
+  return (
+    <Container>
+      <img src="" alt="" />
+
+
+    </Container>
+  )
+}
---
+++ RocketNotes/src/components/Header/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  padding: 20px 45px;
+  border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700}
+  
+`
