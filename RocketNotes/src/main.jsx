7d559da6967b2ee881394800d0f8ff4abf99375f
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-18 01:05:10 -0300

    profile page almost done

--- RocketNotes/src/main.jsx
+++ RocketNotes/src/main.jsx
@@ -6,6 +6,7 @@ import { Home } from './pages/Home';
 import { Details } from './pages/Details'
 import { SignIn } from './pages/Signin';
 import { SignUp } from './pages/SignUp';
+import { Profile } from './pages/Profile';
 
 import { ThemeProvider } from 'styled-components'; // tema global da página
 import theme from './styles/theme';
@@ -15,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <GlobalStyles />
-            <SignUp />
+            <Profile />
         </ThemeProvider>
     </React.StrictMode>
 );
