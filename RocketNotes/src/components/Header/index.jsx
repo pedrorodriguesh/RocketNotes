6f7cc2ec6f88749bb58b3f948e3f39416a0542be
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-18 10:24:18 -0300

    routes done

--- RocketNotes/src/components/Header/index.jsx
+++ RocketNotes/src/components/Header/index.jsx
@@ -6,7 +6,7 @@ export function Header() {
   return(
     <Container>
 
-      <Profile> {/* Profile é outro componente que tem dentro dele uma imagem e então uma div para cuidar da orientação dos textos*/}
+      <Profile to="profile"> {/* Profile é outro componente que tem dentro dele uma imagem e então uma div para cuidar da orientação dos textos*/}
         <img src="https://github.com/pedrorodriguesh.png" alt="Profile photo" />
 
         <div>
--- RocketNotes/src/pages/Profile/index.jsx
+++ RocketNotes/src/pages/Profile/index.jsx
@@ -3,16 +3,17 @@ import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
+import { Link } from 'react-router-dom';
 export function Profile() {
     return (
         <Container>
             <header>
-                <a href="/">
+                <Link to="/" href="/">
                     <FiArrowLeft />
-                </a>
+                </Link>
             </header>
 
-            <Form>
+           <Form>
                 <Avatar>
                     <img
                         src="https://github.com/pedrorodriguesh.png"
