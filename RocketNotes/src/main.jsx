604eec5372334f0ba400d09dc8bc9c57e163ae0f
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-18 09:25:36 -0300

    text area component

--- RocketNotes/src/main.jsx
+++ RocketNotes/src/main.jsx
@@ -7,6 +7,7 @@ import { Details } from './pages/Details'
 import { SignIn } from './pages/Signin';
 import { SignUp } from './pages/SignUp';
 import { Profile } from './pages/Profile';
+import { New } from './pages/New';
 
 import { ThemeProvider } from 'styled-components'; // tema global da página
 import theme from './styles/theme';
@@ -16,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <GlobalStyles />
-            <Profile />
+            <New />
         </ThemeProvider>
     </React.StrictMode>
 );
