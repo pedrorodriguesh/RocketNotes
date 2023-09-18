e7f3b1377a39eb44b1ee28d624577142f0da15ee
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-18 09:12:56 -0300

    profile page done
    q

--- RocketNotes/src/pages/Profile/index.jsx
+++ RocketNotes/src/pages/Profile/index.jsx
@@ -1,48 +1,53 @@
-import { Container, Form } from './styles';
-import { FiArrowLeft, FiUser, FiMail, FiLock} from 'react-icons/fi' 
+import { Container, Form, Avatar } from './styles';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
 export function Profile() {
-    return(
-        
+    return (
         <Container>
             <header>
                 <a href="/">
-                    <FiArrowLeft/>
+                    <FiArrowLeft />
                 </a>
             </header>
 
             <Form>
-                <Input 
-                    placeholder="Nome"
-                    type="text"
-                    icon={FiUser}
-                />
+                <Avatar>
+                    <img
+                        src="https://github.com/pedrorodriguesh.png"
+                        alt="Foto do usuário"
+                    />
+
+                <label htmlFor="avatar">
+                    <FiCamera />
+
+                <input 
+                    id="avatar" 
+                    type="file" />
+                </label>
+                </Avatar>
 
                 <Input 
-                    placeholder="E-mail"
-                    type="text"
-                    icon={FiMail}
-                />
+                    placeholder="Nome" 
+                    type="text" 
+                    icon={FiUser} />
 
                 <Input 
+                    placeholder="E-mail" 
+                    type="text" 
+                    icon={FiMail} />
+
+                <Input
                     placeholder="Senha atual"
                     type="password"
                     icon={FiLock}
                 />
 
-                <Input 
-                    placeholder="Nova senha"
-                    type="password"
-                    icon={FiLock}
-                />
+                <Input placeholder="Nova senha" type="password" icon={FiLock} />
 
-                <Button  title="Salvar"/>
+                <Button title="Salvar" />
             </Form>
-    
         </Container>
-        
-        ) 
-        
-    }
+    );
+}
