71cabdcda3149c3a652686c9ad0e2260f76a332c
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-27 09:35:56 -0300

    signup in db

--- RocketNotes/src/pages/SignUp/index.jsx
+++ RocketNotes/src/pages/SignUp/index.jsx
@@ -1,15 +1,43 @@
 import { Container, Form, Background } from './styles';
 
-import { FiLogIn, FiMail, FiLock, FiUser } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import { api } from '../../services/api';
+
+import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 export function SignUp() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    function handleSignUp() {
+        if (!name || !email || !password) {
+            return alert('Preencha todos os campos!');
+        }
+
+        api.post('/users', { name, email, password })
+            .then(() => {
+                alert('Usuário cadastrado com sucesso!');
+                navigate("/")
+            })
+            .catch((error) => {
+                if (error.response) {
+                    alert(error.response.data.message);
+                } else {
+                    alert('Não foi possível cadastrar.');
+                }
+            });
+    }
+
     return (
         <Container>
-
             <Background />
             <Form>
                 <h1>Rocket Notes</h1>
@@ -17,14 +45,30 @@ export function SignUp() {
 
                 <h2>Crie sua conta</h2>
 
-                <Input placeholder="Nome" type="text" icon={FiUser} />
-                <Input placeholder="E-mail" type="text" icon={FiMail} />
-                <Input placeholder="Senha" type="password" icon={FiLock} />
+                <Input
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
 
-                <Button title="Cadastrar"/>
-                
+                <Button title="Cadastrar" onClick={handleSignUp} />
 
-                <Link to="/" href="#">Voltar para o Login</Link>
+                <Link to="/" href="#">
+                    Voltar para o Login
+                </Link>
             </Form>
         </Container>
     );
