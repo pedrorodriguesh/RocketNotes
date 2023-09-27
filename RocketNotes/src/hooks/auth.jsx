1600341eadef2beaa1bdb78726309cb88b92b3e5
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-09-27 15:41:17 -0300

    authentication hook

--- RocketNotes/src/hooks/auth.jsx
+++ RocketNotes/src/hooks/auth.jsx
@@ -1,17 +1,40 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
+import { api } from '../services/api'
 
-export const AuthContext = createContext({})
+export const AuthContext = createContext({});
 
-export function AuthProvider ({ children }){
+export function AuthProvider({ children }) {
+    const [data, setData] = useState({})
+
+    async function signIn({ email, password }) {
+
+        try{
+            const response = await api.post("/sessions", { email, password })
+            const { user, token } = response.data
+
+            api.defaults.headers.authorization = `Bearer ${token}`
+            setData({ user, token })
+            
+        }catch (error){
+            if(error.response){
+                alert(error.response.data.message)
+            } else {
+                alert("Não foi possível entrar!")
+            }
+        }
+    }
+
     return (
-        <AuthContext.Provider value={{ name: 'Pedro', email: 'pedro@email.com'}}>
+        <AuthContext.Provider
+            value={{ signIn, user: data.user }}
+        >
             {children}
         </AuthContext.Provider>
-    )
+    );
 }
 
-export function useAuth(){
+export function useAuth() {
     const context = useContext(AuthContext);
 
-    return context
+    return context;
 }
--- RocketNotes/src/pages/SignIn/index.jsx
+++ RocketNotes/src/pages/SignIn/index.jsx
@@ -1,5 +1,6 @@
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import { useAuth } from '../../hooks/auth'
 
@@ -9,8 +10,16 @@ import { Button } from '../../components/Button';
 import { Container, Form, Background } from './styles';
 
 export function SignIn() {
-    const data = useAuth()
-    console.log("Meu contexto =>", data)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const { signIn } = useAuth()
+    
+    function handleSignIn(){
+        signIn({ email, password })
+    }
+
     return (
         <Container>
             <Form>
@@ -19,11 +28,11 @@ export function SignIn() {
 
                 <h2>Faça seu login</h2>
 
-                <Input placeholder="Email" type="text" icon={FiMail} />
+                <Input placeholder="Email" type="text" icon={FiMail} onChange={e => setEmail(e.target.value)}/>
 
-                <Input placeholder="Senha" type="password" icon={FiLock} />
+                <Input placeholder="Senha" type="password" icon={FiLock} onChange={e => setPassword(e.target.value)}/>
 
-                <Button title="Entrar" />
+                <Button title="Entrar" onClick={handleSignIn}/>
 
                 <Link to="/register" href="#">
                     Criar conta
