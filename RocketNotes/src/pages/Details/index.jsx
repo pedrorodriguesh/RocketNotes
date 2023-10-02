001534dae2e9ee665002f7704776473d9e3e1ae4
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-02 20:19:47 -0300

    delete note

--- RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/src/pages/Details/index.jsx
@@ -1,5 +1,8 @@
 import { Container, Links, Content } from './styles';
 
+import { useParams, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 // ### Importando Componentes
 import { ButtonText } from '../../components/ButtonText';
 import { Section } from '../../components/Section';
@@ -7,45 +10,77 @@ import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Tag } from '../../components/Tag';
 
+import { api } from '../../services/api';
+
 export function Details() {
+    const [data, setData] = useState(null);
+
+    const params = useParams();
+    const navigate = useNavigate()
+
+    async function handleRemove(){
+        const confirm = window.confirm("Deseja realmente remover a nota?")
+
+        if(confirm){
+            await api.delete(`/notes/${params.id}`)
+            navigate('/')
+        }
+    }
+
+    function handleBack(){
+        navigate('/')
+    }
+
+    useEffect(() => {
+        async function fetchNote() {
+            const response = await api.get(`/notes/${params.id}`);
+            setData(response.data);
+        }
+
+        fetchNote();
+    }, []);
+
     return (
         <Container>
             <Header></Header>
 
+           { 
+           data && 
             <main>
                 <Content>
-                    <ButtonText title="Excluir Nota" />
+                    <ButtonText title="Excluir Nota" onClick={handleRemove} />
 
-                    <h1>Introdução ao React</h1>
+                    <h1>{data.title}</h1>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Esse quibusdam amet nam deleniti enim neque
-                        dolorem, asperiores quidem ad maiores quaerat,
-                        cupiditate, eveniet earum mollitia nulla ipsam fugiat
-                        porro minus.
+                       {data.description}
                     </p>
 
-                    <Section title="Links úteis">
+                    {data.links &&
+                        <Section title="Links úteis">
                         <Links>
-                            <li>
-                                <a href="#">
-                                    https://www.github.com/pedrorodriguesh
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">https://rocketseat.com.br/</a>
-                            </li>
+                            {
+                            data.links.map(link => (                
+                            <li key={String(link.id)}><a target='_blank' href={link.url}>{link.url}</a></li>
+                            )) 
+                            }
                         </Links>
                     </Section>
+                    }
 
-                    <Section title="Marcadores">
-                        <Tag title="Express" />
-                        <Tag title="Nodejs" />
+                   { data.tags &&
+                   <Section title="Marcadores">
+                        {
+                            data.tags.map(tag => (
+                            <Tag key={String(tag.id)} title={tag.name} />
+                        ))
+                        }
                     </Section>
+                    }
 
-                    <Button title="Voltar" />
+                    <Button title="Voltar" onClick={handleBack} />
                 </Content>
             </main>
+            }
         </Container>
     );
 }
