2bdf32770aba62d7fcbaeb18b8c3119166b75393
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-02 10:29:05 -0300

    links and tags

--- RocketNotes/src/pages/New/index.jsx
+++ RocketNotes/src/pages/New/index.jsx
@@ -6,10 +6,35 @@ import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { Link } from 'react-router-dom';
 
+import { useState } from 'react';
 
 import { Container, Form } from './styles';
 
 export function New() {
+    const [links, setLinks] = useState([]);
+    const [newLink, setNewLink] = useState('');
+
+    const [tags, setTags] = useState([]);
+    const [newTag, setNewTag] = useState('');
+
+    function handleAddLink() {
+        setLinks((prevState) => [...prevState, newLink]);
+        setNewLink('');
+    }
+
+    function handleRemoveLink(deleted) {
+        setLinks((prevState) => prevState.filter((link) => link !== deleted));
+    }
+
+    function handleAddTag() {
+        setTags((prevState) => [...prevState, newTag]);
+        setNewTag('');
+    }
+
+    function handleRemoveTag(deleted) {
+        setTags(prevState => prevState.filter(tag => tag !== deleted))
+    }
+
     return (
         <Container>
             <Header />
@@ -18,7 +43,9 @@ export function New() {
                 <Form>
                     <header>
                         <h1>Criar nota</h1>
-                        <Link to="/"href="/">voltar</Link>
+                        <Link to="/" href="/">
+                            voltar
+                        </Link>
                     </header>
 
                     <Input placeholder="Título" />
@@ -26,19 +53,43 @@ export function New() {
                     <Textarea placeholder="Observações" />
 
                     <Section title="Links Úteis">
-                        <NoteItem value="https://rocketseat.com.br" />
-                        <NoteItem isNew placeholder="Novo Link" />
+                        {links.map((link, index) => (
+                            <NoteItem
+                                key={String(index)}
+                                value={link}
+                                onClick={() => handleRemoveLink(link)}
+                            />
+                        ))}
+                        <NoteItem
+                            isNew
+                            placeholder="Novo Link"
+                            value={newLink}
+                            onChange={(e) => setNewLink(e.target.value)}
+                            onClick={handleAddLink}
+                        />
                     </Section>
 
                     <Section title="Marcadores">
-                        <div className='tags'>
-                        <NoteItem value="react" />
-                        <NoteItem isNew placeholder="Nova tag"/>
+                        <div className="tags">
+                            {tags.map((tag, index) => (
+                                <NoteItem
+                                    key={String(index)}
+                                    value={tag}
+                                    onClick={() => handleRemoveTag(tag)}
+                                />
+                            ))}
 
+                            <NoteItem
+                                isNew
+                                placeholder="Nova tag"
+                                onChange={(e) => setNewTag(e.target.value)}
+                                value={newTag}
+                                onClick={handleAddTag}
+                            />
                         </div>
                     </Section>
 
-                    <Button title="Salvar"/>
+                    <Button title="Salvar" />
                 </Form>
             </main>
         </Container>
