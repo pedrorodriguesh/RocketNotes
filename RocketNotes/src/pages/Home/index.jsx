46190d228079b019bd40688c6ed0ba37b2436922
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-10-03 10:53:40 -0300

    comment

--- RocketNotes/src/pages/Home/index.jsx
+++ RocketNotes/src/pages/Home/index.jsx
@@ -14,6 +14,7 @@ import { Button } from '../../components/Button';
 import { api } from '../../services/api.js';
 
 export function Home() {
+    // ### Setando estados para mandar para o backend.
     const [tags, setTags] = useState([]);
     const [tagsSelected, setTagsSelected] = useState([]);
     const [search, setSearch] = useState('')
